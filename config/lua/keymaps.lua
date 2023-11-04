2acc9f6a011f74fa7c2d4a13144bd0e6792ed51e
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-11-04 09:05:09 -0500

    remap <C-\><C-n> to <Esc><Esc>

--- config/lua/keymaps.lua
+++ config/lua/keymaps.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 
+-- exit terminal mode
+vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-\><C-n>', { noremap = true, silent = true })
+
