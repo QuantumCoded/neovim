43674d5e1a845cd91f33f496b50ed49b8ee473a2
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-11-05 16:04:22 -0600

    add hotkeys for :q and :w

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,8 +1,14 @@
 -- leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set('', '<Space>', '<Nop>', { noremap = true, silent = true })
 
 -- exit terminal mode
-vim.api.nvim_set_keymap('t', '<M-Esc>', '<C-Bslash><C-n>', { noremap = true, silent = true })
+vim.keymap.set('t', '<M-Esc>', '<C-Bslash><C-n>', { noremap = true, silent = true })
+
+-- save current buffer
+vim.keymap.set('n', '<leader>w', ':w', { noremap = true, silent = true })
+
+-- quit current buffer
+vim.keymap.set('n', '<leader>q', ':q', { noremap = true, silent = true })
 
