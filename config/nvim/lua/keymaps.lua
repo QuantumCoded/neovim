582c80c1e443a536cb5e34c4102da75dfcfc7cd5
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-11-04 19:10:38 -0500

    change the <Esc><Esc> keymap to <M-Esc>

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -4,5 +4,5 @@ vim.g.maplocalleader = " "
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 
 -- exit terminal mode
-vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-Bslash><C-n>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<M-Esc>', '<C-Bslash><C-n>', { noremap = true, silent = true })
 
