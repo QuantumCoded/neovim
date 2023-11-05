dcf1245cfca4a5a617b3de662cfdcd03f1a60f80
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-11-05 16:36:38 -0600

    fix keymaps for saving and closing

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set('', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.keymap.set('t', '<M-Esc>', '<C-Bslash><C-n>', { noremap = true, silent = true })
 
 -- save current buffer
-vim.keymap.set('n', '<leader>w', ':w', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>w', '<Cmd>w<CR>', { noremap = true, silent = true })
 
 -- quit current buffer
-vim.keymap.set('n', '<leader>q', ':q', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q', '<Cmd>q<CR>', { noremap = true, silent = true })
 
